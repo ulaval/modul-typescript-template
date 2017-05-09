eaef0c8de1d664115a41a3d3494674496191063e
Martin Simard <masim144@DTI-P-MASIM144.dti.ulaval.ca>
2017-05-09 14:14:57 -0400

    Support for e2e tests with Nightwatch and Selenium

---
+++ tests/e2e/runner.js
@@ -0,0 +1,64 @@
+const cp = require('child_process');
+const path = require('path');
+const util = require('util');
+const npmWhich = require('npm-which');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+
+const createWebpackServer = config => {
+    const webpackConfig = require(config.webpackConfig);
+
+    return new WebpackDevServer(webpack(webpackConfig), { quiet: false });
+};
+
+const getConfig = options => {
+    let nightwatchConfig = path.resolve(process.cwd(), 'conf/nightwatch.conf.js');
+
+    const config = Object.assign({
+        nightwatchConfig,
+        nightwatchEnv: 'chrome',
+        webpackConfig: path.resolve(process.cwd(), 'conf/webpack.config.js'),
+        port: 5000,
+        tunnelIdentifier: undefined
+    }, options);
+
+    console.log(`Running with config:\n${util.inspect(config, { depth: 0, colors: true })}`);
+
+    return Object.assign({}, config, {
+        server: createWebpackServer(config)
+    });
+};
+
+module.exports = options => {
+    const config = getConfig(options);
+
+    const startServer = callback => {
+        config.server.listen(config.port, '0.0.0.0', callback);
+    }
+
+    const onServerStarted = seleniumChild => err => {
+        if (err) {
+            console.error(err.stack);
+            process.exit(1);
+        }
+
+        const args = [
+            '--config', config.nightwatchConfig,
+            '--env', config.nightwatchEnv
+        ];
+
+        var spawn = require('cross-spawn');
+
+        spawn('./node_modules/.bin/nightwatch', args, { stdio: 'inherit' })
+            .on('error', err2 => {
+                config.server.close();
+                throw err2;
+            })
+            .on('exit', code => {
+                config.server.close();
+                process.exit(code);
+            });
+    };
+
+    startServer(onServerStarted());
+};
---
+++ tests/e2e/specs/page1.spec.js
@@ -0,0 +1,16 @@
+module.exports = {
+    'default e2e tests': function test(browser) {
+        // automatically uses dev Server port from /config.index.js
+        // default: http://localhost:8080
+        // see nightwatch.conf.js
+        const devServer = browser.globals.devServerURL;
+
+        browser
+            .url(devServer)
+            .waitForElementVisible('#main', 5000)
+            .assert.containsText('h1', 'Welcome to University Laval starter app!')
+            .assert.elementCount('p', 2)
+            .assert.elementCount('a', 1)
+            .end();
+    }
+};
